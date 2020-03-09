b289fb32647cadc4e91dfd5c8274d1e698a037fd
NikiRyomHansen <nikihansen@hotmail.dk>
2020-03-09 19:30:44 -0400

    Added config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// require dotenv to work with the heroku env variables and load them into process.env
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// export the Atlas cloud username and password - these are obtained from the env variables pushed to Heroku
+module.exports.username = process.env.ATLAS_CLOUD_PASS;
+module.exports.password = process.env.ATLAS_CLOUD_USER;
