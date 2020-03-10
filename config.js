f9ab632c834b4aae20d6eab20b9838e35aa549b7
NikiRyomHansen <nikihansen@hotmail.dk>
2020-03-09 20:05:16 -0400

    Swapped two incorrect variables

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // export the Atlas cloud username and password - these are obtained from the env variables pushed to Heroku
-module.exports.username = process.env.ATLAS_CLOUD_PASS;
-module.exports.password = process.env.ATLAS_CLOUD_USER;
+module.exports.username = process.env.ATLAS_CLOUD_USER;
+module.exports.password = process.env.ATLAS_CLOUD_PASS;
