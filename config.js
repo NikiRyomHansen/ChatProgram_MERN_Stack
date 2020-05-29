af0417c80895f74dc5bc90e96148af16ac0a3e0d
NikiRyomHansen <nikihansen@hotmail.dk>
2020-05-29 03:48:29 +0200

    Removed a comment

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ if (process.env.NODE_ENV !== 'production')
     require('dotenv').config();
 
 // export the Atlas cloud username and password - these are obtained from the env variables pushed to Heroku
-// But is temporarily unavailable due to the application not being fully integrated to my heroku domain.
 module.exports.username = process.env.ATLAS_CLOUD_USER;
 module.exports.password = process.env.ATLAS_CLOUD_PASS;
