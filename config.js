d5f5af8a4cc31376f37ce2fa8d93e9ec9e26ccde
NikiRyomHansen <nikihansen@hotmail.dk>
2020-05-29 03:21:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,5 @@ dotenv.config();
 
 // export the Atlas cloud username and password - these are obtained from the env variables pushed to Heroku
 // But is temporarily unavailable due to the application not being fully integrated to my heroku domain.
-// module.exports.username = process.env.ATLAS_CLOUD_USER;
-// module.exports.password = process.env.ATLAS_CLOUD_PASS;
-
-// temporary username and password for the database access
-module.exports.username = "nikiryom";
-module.exports.password = "YESiXuBlo6JBPVuZ";
+module.exports.username = process.env.ATLAS_CLOUD_USER;
+module.exports.password = process.env.ATLAS_CLOUD_PASS;
